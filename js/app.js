579ebcef24e46d0d21a3ba5e5f8a03fde4c930b7
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-13 06:40:53 -0300

    Se continua siguiendo la logica de la version anterior al trabajo practico actual

--- js/app.js
+++ js/app.js
@@ -1,38 +1,31 @@
+let formulario = document.querySelector("form");
+let inputUsuario = document.querySelector("input");
+
+formulario.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const usuario = inputUsuario.value.toLowerCase();
+  const pc = turnoPC();
+  const resultado = jugar(usuario, pc);
+  alert(`PC eligió ${pc}. ${resultado}`);
+  inputUsuario.value = "";
+});
 
 function turnoPC() {
-    const opciones = ["piedra", "papel", "tijera"];
-    return opciones[Math.floor(Math.random() * opciones.length)];
+  const opciones = ["piedra", "papel", "tijera"];
+  return opciones[Math.floor(Math.random() * opciones.length)];
+}
+
+function jugar(usuario, pc) {
+  if (usuario === pc) {
+    return "empate";
+  } else if (
+    (usuario === "piedra" && pc === "tijera") ||
+    (usuario === "papel" && pc === "piedra") ||
+    (usuario === "tijera" && pc === "papel")
+  ) {
+    return "ganaste";
+  } else {
+    return "perdiste";
   }
-  
-  function jugar(usuario, pc) {
-    if (usuario === pc) {
-      return "empate";
-    } else if (
-      (usuario === "piedra" && pc === "tijera") ||
-      (usuario === "papel" && pc === "piedra") ||
-      (usuario === "tijera" && pc === "papel")
-    ) {
-      return "ganaste";
-    } else {
-      return "perdiste";
-    }
-  }
-  
-  function jugarOtraVez() {
-    do {
-      let usuario;
-      do {
-        usuario = prompt("Ingrese su movimiento: piedra, papel o tijera").toLowerCase();
-        if (!["piedra", "papel", "tijera"].includes(usuario)) {
-          alert("Opción no válida. Por favor, ingrese piedra, papel o tijera.");
-        }
-      } while (!["piedra", "papel", "tijera"].includes(usuario));
-  
-      const pc = turnoPC();
-      const resultado = jugar(usuario, pc);
-      alert(`PC eligió ${pc}. ${resultado}`);
-    } while (confirm("¿Quieres jugar otra vez?"));
-  }
-  
-  // Iniciar el juego
-  jugarOtraVez();
+}
+
